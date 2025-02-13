351d9201e6509a3d703e33b1de433861e6e1baf3
ParkHyeonkyu <162525426+ParkHyeonkyu@users.noreply.github.com>
2025-02-14 01:16:40 +0900

    feat: Car class 구현
    Car 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    const randomNumber = Random.pickNumberInRange(0,9);
+    if (randomNumber >= 4) {
+      this.position++;
+    }
+  }
+
+  get nowPosition() {
+    return "-".repeat(this.position);
+  }
+}
+
 class App {
-  async run() {}
+  async play() {
+    try{
+      const carName = await Console.readLineAsync(`자동차 이름을 입력하세요.: \n`);
+      const car = new Car(carName);
+      Console.print("자동차 이름: " + car.name);
+      car.move();
+      Console.print(car.position);
+    }catch(error){
+      throw error;
+    }
+  }
 }
 
 export default App;
