c67a9a6e203b46ffee4ec2dda965b3149c2de8ea
ParkHyeonkyu <162525426+ParkHyeonkyu@users.noreply.github.com>
2025-02-14 11:42:36 +0900

    refactor: playRound 함수 추가 및 test 통과 코드
    복잡한 구조를 피하기 위해 gameStart메서드의 기능을 playRound로 분리하고,
    코드가 ApplicationTest.js를 통과할 수 있도록 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Console, Random } from "@woowacourse/mission-utils";
 class Car {
   constructor(name) {
     if (name.length < 1) {
-      throw new Error("이름은 최소 1자 이상입니다.")
+      //이름은 최소 1자 이상입니다.
+      throw new Error("[ERROR]");
     }
     if (name.length > 5) {
-      throw new Error("이름은 5자 이하만 가능합니다.")
+      //이름은 5자 이하만 가능합니다.
+      throw new Error("[ERROR]");
     }
     this.name = name;
     this.position = 0;
   }
   move() {
-    const randomNumber = Random.pickNumberInRange(0,9);
+    const randomNumber = Random.pickNumberInRange(0, 9);
     if (randomNumber >= 4) {
       this.position++;
     }
@@ -24,46 +26,57 @@ class Car {
 
 class RacingGame {
   constructor(carNames, counts) {
-
     const nameArray = carNames.split(",").map((name) => name.trim());
 
     const nameCheck = new Set(nameArray);
-    if(nameCheck.size !== nameArray.length) {
-      throw new Error("중복된 이름이 있습니다. 모든 자동차 이름은 고유해야 합니다.")
+    if (nameCheck.size !== nameArray.length) {
+      throw new Error(
+        //중복된 이름이 있습니다. 모든 자동차 이름은 고유해야 합니다.
+        "[ERROR]"
+      );
     }
 
     this.cars = nameArray.map((name) => new Car(name));
     this.counts = counts;
   }
 
+  playRound() {
+    this.cars.forEach((car) => {
+      car.move();
+      Console.print(`${car.name} : ${car.nowPosition}`);
+    });
+  }
+
   gameStart() {
     for (let i = 0; i < this.counts; i++) {
       Console.print(`\n${i + 1}차 시도`);
-      this.cars.forEach(car => {
-        car.move();
-        Console.print(`${car.name} : ${car.nowPosition}`);        
-      });
+      this.playRound();
     }
   }
 
-  get winner(){
+  get winner() {
     const maxPosition = Math.max(...this.cars.map((car) => car.position));
     const winners = this.cars.filter((car) => car.position === maxPosition);
     return winners.map((winner) => winner.name).join(", ");
   }
-
 }
 
 class App {
   async run() {
-    try{
-      const cars = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n`);
-      if(!cars){
-        throw new Error("이름이 입력되지 않았습니다.")
+    try {
+      const cars = await Console.readLineAsync(
+        `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n`
+      );
+      if (!cars) {
+        //이름이 입력되지 않았습니다.
+        throw new Error("[ERROR]");
       }
-      const inputCounts = await Console.readLineAsync("시도할 회수는 몇회인가요?")
-      if(inputCounts < 1){
-        throw new Error("최소 1회 이상 시도해야 합니다.")
+      const inputCounts = await Console.readLineAsync(
+        "시도할 회수는 몇회인가요?"
+      );
+      if (inputCounts < 1) {
+        //최소 1회 이상 시도해야 합니다.
+        throw new Error("[ERROR]");
       }
       const counts = parseInt(inputCounts);
 
@@ -71,8 +84,7 @@ class App {
       game.gameStart();
 
       Console.print("최종 우승자 : " + game.winner);
-
-    }catch(error){
+    } catch (error) {
       throw error;
     }
   }
